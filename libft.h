690227dd8f086b65507eda46604f920cfe7a9b7d
Mateo <mateocorso@gmail.com>
2021-11-23 22:20:43 +0100

    adding ft_memcpy

--- libft.h
+++ libft.h
@@ -9,6 +9,7 @@
     int     ft_isascii(int c);
     int     ft_isdigit(int c);
     int     ft_isprint(int c);
+    void    *ft_memcpy(void *dst, const void *src, size_t n);
     void    *ft_memset(void *b, int c,size_t len);
     size_t  ft_strlen(const char *s);
 
