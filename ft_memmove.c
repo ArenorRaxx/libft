6885312839d45745c03e58af622ad6619a455022
Mateo <mateocorso@gmail.com>
2021-11-23 22:24:52 +0100

    adding ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 22:21:38 by mcorso            #+#    #+#             */
+/*   Updated: 2021/11/23 22:24:16 by mcorso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memmove(void *dst, const void *src, size_t len) {
+    unsigned char *str;
+    unsigned char *dest;
+    
+    str = (unsigned char *)src;
+    dest = (unsigned char *)dst;
+    while (len-- > 0)
+        *(dest + len) = *(str + len);
+    return (dst);
+}
