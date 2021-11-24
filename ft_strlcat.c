6e1fd4fac35fdd349f9c0060d8ee9eff9b61c27c
Mateo <mateocorso@gmail.com>
2021-11-24 15:18:47 +0100

    adding ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 14:50:31 by mcorso            #+#    #+#             */
+/*   Updated: 2021/11/24 15:17:37 by mcorso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize) {
+    unsigned int    i;
+    unsigned int    len;
+    unsigned char   *str;
+    unsigned char   *dest;
+
+    len = ft_strlen(dst);
+    i = dstsize - len;
+    dest = (unsigned char *)(dst + len);
+    str = (unsigned char *)src;
+    while (--i)
+        *dest++ = *str++;
+    *dest = '\0';
+    return (len + len - i);
+}
--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@
     void    *ft_memmove(void *dst, const void *src, size_t len);
     void    *ft_memset(void *b, int c,size_t len);
     size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
+    size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
     size_t  ft_strlen(const char *s);
 
 #endif
