3906e2a8592792308739c4b1681451c6b72fdeba
Mateo <mateocorso@gmail.com>
2021-11-26 17:53:58 +0100

    finishing ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 21:14:37 by mcorso            #+#    #+#             */
-/*   Updated: 2021/11/24 21:35:52 by mcorso           ###   ########.fr       */
+/*   Updated: 2021/11/26 17:53:39 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 int	ft_atoi(const char *str)
 {
-	unsigned int	i;
-	unsigned int	array_len;
-	unsigned int	ret_val;
+	int		i;
+	int		sign;
+	long	ret_val;
+	size_t	array_len;
 
 	i = 0;
+	sign = 1;
 	ret_val = 0;
 	array_len = ft_strlen(str);
+	if (*str == '-')
+	{
+		sign *= -1;
+		i++;
+	}
 	while (i < array_len)
-		ret_val = (ret_val * 10) + (*(str + i++) - 48);
-	return (ret_val);
+		ret_val = (ret_val * 10) + ((long *)(str + i++) - 48);
+	return ((int)(ret_val * sign));
 }
