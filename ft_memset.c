a6930530c650efd12093838ad4a3fdd17964e2d8
Mateo <mateocorso@gmail.com>
2021-11-26 18:13:33 +0100

    quick bug fix

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 21:38:12 by mcorso            #+#    #+#             */
-/*   Updated: 2021/11/24 16:13:15 by mcorso           ###   ########.fr       */
+/*   Updated: 2021/11/26 18:13:16 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*str;
 	unsigned char	chr;
 
-	*str = (unsigned char *)b;
+	str = (unsigned char *)b;
 	chr = (unsigned char)c;
 	while (len--)
 		*(str++) = chr;
