406378bfc8f33c179aa6a48eb42f31230758a28c
Mateo <mateocorso@gmail.com>
2021-11-30 17:37:44 +0100

    adding ft_itoa (finished)

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,32 +6,33 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 04:47:15 by mcorso            #+#    #+#             */
-/*   Updated: 2021/11/30 06:05:12 by mcorso           ###   ########.fr       */
+/*   Updated: 2021/11/30 17:37:38 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 #include <stdio.h>
+
 static int	how_many_digits(int n)
 {
-	int	i;
-	int	sign;
+	int		i;
+	long	num;
 
 	i = 0;
-	sign = 1;
+	num = n;
+	if (n == 0)
+		return (1);
 	if (n < 0)
 	{
-		n *= -1;
-		sign *= -1;
+		num *= -1;
 		i++;
 	}
-	while (n > 0)
+	while (num > 0)
 	{
-		n /= 10;
+		num /= 10;
 		i++;
 	}
-	return (i * sign);
+	return (i);
 }
 
 char	*ft_itoa(int n)
@@ -39,35 +40,34 @@ char	*ft_itoa(int n)
 	int		sign;
 	int		nbcar;
 	char	*int_ret;
+	long	num;
 
+	num = (long)n;
 	sign = 1;
 	nbcar = 0;
-	if (n == 0)
-		return ("0");
 	nbcar = how_many_digits(n);
-	if (nbcar < 0)
-	{
-		(unsigned )n *= -1;
-		nbcar *= -1;
+	if (n < 0)
 		sign *= -1;
-	}
-	int_ret = (char *)ft_calloc(nbcar, sizeof(char));
+	int_ret = (char *)ft_calloc(nbcar + 1, sizeof(char));
+	if (n == 0)
+		int_ret[0] = '0';
 	if (!int_ret)
 		return (NULL);
-	while (n > 0)
+	while (nbcar && n)
 	{
-		int_ret[--nbcar] = (n % 10) + '0';
-		n /= 10;
+		int_ret[--nbcar] = ((num * sign) % 10) + '0';
+		num /= 10;
 	}
 	if (sign < 0)
-	{
-		int_ret[--nbcar] = '-';
-	}
-	printf("%s\n", int_ret);
+		int_ret[nbcar] = '-';
 	return (int_ret);
 }
 
-int main(void)
-{
-	char *a = ft_itoa(-2147483648);
-}
+// int main()
+// {
+// 	char *a = ft_itoa(9);
+// 	char *b = ft_itoa(-9);
+// 	char *c = ft_itoa(8415);
+// 	char *d = ft_itoa(-8415);	
+// 	printf("%s, %s, %s, %s\n", a, b, c, d);
+// }
