00c178c2a54e4ea41942166bd390059ced02fc4c
Mateo <mateocorso@gmail.com>
2021-11-30 22:36:41 +0100

    minor change

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mcorso <mcorso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 04:15:35 by mcorso            #+#    #+#             */
-/*   Updated: 2021/11/29 16:27:17 by mcorso           ###   ########.fr       */
+/*   Updated: 2021/11/30 22:32:34 by mcorso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,17 @@ static int	ft_strcontain(const char *s1, const char c)
 	return (0);
 }
 
+static char	*ft_empty_string(void)
+{
+	char	*str;
+
+	str = malloc(sizeof(char));
+	if (!str)
+		return (NULL);
+	*str = 0;
+	return (str);
+}
+
 char	*ft_strtrim(const char *s1, const char *set)
 {
 	char	*str;
@@ -30,7 +41,7 @@ char	*ft_strtrim(const char *s1, const char *set)
 		s1++;
 	start = (char *)s1;
 	if (*start == 0)
-		return (start);
+		return (ft_empty_string());
 	while (*s1)
 		s1++;
 	while (--s1 != start && ft_strcontain(set, *s1))
